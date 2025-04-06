7d3c2bf65e3abd0d1304d92c8bae92fae9f9facc
Loyan <lapetitbulle@gmail.com>
2025-04-06 11:12:07 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,23 @@
-import { defineStackbitConfig } from '@stackbit/types';
+// stackbit.config.ts
+import {
+  defineStackbitConfig,
+  getLocalizedFieldForLocale,
+  SiteMapEntry
+} from "@stackbit/types";
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "nuxt",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+  stackbitVersion: "~0.6.0",
+  siteMap: ({ documents, models }) => {
+    const pageModels = models.filter(m => m.type === "page").map(m => m.name);
+    return documents
+      .filter(d => pageModels.includes(d.modelName))
+      .map(document => {
+        // ...
+        return {
+          document
+          // ...
+        };
+      })
+      .filter(Boolean) as SiteMapEntry[];
+  }
+});
